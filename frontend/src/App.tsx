eebc68223dfaf3f395c3d5237fcfe2a43dcdd3c1
Danilo Gabriel <danilogabrielss15@gmail.com>
2021-05-07 11:11:29 -0300

    barchart adicionado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -7,7 +8,20 @@ function App() {
     <div className="container">
       <>
         <NavBar />
-        <h1 className="text-primary">Ola mundo!</h1>
+        <h1 className="text-primary py-3">Dashborad de Vendas</h1>
+        <div className="row px-3">
+          <div className=" col=-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+            <BarChart />
+          </div>
+          <div className=" col=-sm-6">
+            <h5 className="text-center text-secondary">Todas vendas</h5>
+            <BarChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
         <DataTable />
         <Footer />
       </>
